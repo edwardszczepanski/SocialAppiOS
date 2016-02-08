856851c34d519b59ddc473a7ff204da4ade66c20
edwardszczepanski <eszczepa@uoregon.edu>
2016-02-07 18:23:32 -0800

    Pulls from MySQL Database and displays in ListView

--- SocialApp/ViewController.swift
+++ SocialApp/ViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, HomeModelProtocal  {
+    
+    //Properties
+    
+    var feedItems: NSArray = NSArray()
+    var selectedLocation : LocationModel = LocationModel()
+    @IBOutlet weak var listTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //set delegates and initialize homeModel
+        
+        self.listTableView.delegate = self
+        self.listTableView.dataSource = self
+        
+        let homeModel = HomeModel()
+        homeModel.delegate = self
+        homeModel.downloadItems()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func itemsDownloaded(items: NSArray) {
+        
+        feedItems = items
+        self.listTableView.reloadData()
     }
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of feed items
+        return feedItems.count
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Retrieve cell
+        let cellIdentifier: String = "BasicCell"
+        let myCell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)!
+        // Get the location to be shown
+        let item: LocationModel = feedItems[indexPath.row] as! LocationModel
+        // Get references to labels of cell
+        myCell.textLabel!.text = item.address
+        
+        return myCell
+    }
+    
 }
-
